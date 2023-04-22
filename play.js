009c44cfb85a6b5157f744cf46d9aa59cc54b563
anil <anil72.rana@gmail.com>
2023-04-22 20:39:20 +0545

    to fix features

--- play.js
+++ play.js
@@ -1,6 +1,2 @@
 
-document.getElementsById("box1").onclick=function(){
-    const img=document.createElement("img");
-    img.src="images/t.svg";
-    document.getElementById("box9").body.appendChild(img);
-}
+
