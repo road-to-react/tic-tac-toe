3bfe339272eac7200f904d0186cd8d0d20ba7a63
anil <anil72.rana@gmail.com>
2023-04-22 22:42:55 +0545

    svg changed

--- play.js
+++ play.js
@@ -1,22 +1,25 @@
 
-var turn = 1;
-        var id = "hello";
+var turn = 1; //for selecting player turn
+var id = ""; //container for element Id
 
-        Array.from(document.getElementsByClassName("col-box")).forEach((box) => {
-            box.addEventListener("click", function (e) {
-                const img = document.createElement("img");
-                img.className = "symbol";
-                if (turn % 2 == 0) {
-                    img.src = "images/t.svg";
-                    console.log("tick");
-                } else {
-                    img.src = "images/c.svg";
-                    console.log("circ");
-                }
-                const hasChild = box.hasChildNodes();
-                if (!hasChild) {
-                    box.appendChild(img);
-                    turn++;
-                }
-            });
-        });
+function selectImage(turn) {
+    const img = document.createElement("img");
+    img.className = "symbol";
+    if (turn % 2 == 0) {
+        img.src = "images/cross.svg";
+    } else {
+        img.src = "images/circle.svg";
+    }
+    return img;
+}
+
+Array.from(document.getElementsByClassName("col-box")).forEach((box) => {
+    box.addEventListener("click", function (e) {
+        var img = selectImage(turn);
+        const hasChild = box.hasChildNodes();
+        if (!hasChild) {
+            box.appendChild(img);
+            turn++;
+        }
+    });
+});
