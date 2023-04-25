f738797baf554feefbc58ffe0d325bf359738e64
anil <anil72.rana@gmail.com>
2023-04-25 20:05:31 +0545

    feat: added replay feature

--- play.js
+++ play.js
@@ -3,8 +3,11 @@ var turn = 1; //for selecting, player turn
 var pos1 = [];
 var pos2 = [];
 var match = 0;
-var bingo = 0;
+var finish = 0;
 var bingo = [["123"], ["369"], ["789"], ["147"], ["159"], ["357"], ["258"], ["456"]];
+document.getElementById("player-1").innerHTML = "Player 1 [Circle]";
+        document.getElementById("player-2").innerHTML = " Player 2 [Cross]";
+
 
 function selectImage(turn) {
     const img = document.createElement("img");
@@ -22,9 +25,8 @@ Array.from(document.getElementsByClassName("col-box")).forEach((box) => {
         var img = selectImage(turn);
         var position = box.id.slice(3)
 
-
         const hasChild = box.hasChildNodes();
-        if (!hasChild && bingo != 1) {
+        if (!hasChild && bingo != 1 && finish!=1) {
             box.appendChild(img);
             if (turn % 2 == 0) {
                 pos2.push(position);
@@ -36,7 +38,9 @@ Array.from(document.getElementsByClassName("col-box")).forEach((box) => {
                         }
                         if (match == 3) {
                             console.log("Player 2 won the game.");
-                            bingo = 1;
+                            document.getElementById("player-1").innerHTML = "Player 1 LOSE [circle]";
+                            document.getElementById("player-2").innerHTML = "Player 2 WON [Cross]";
+                            finish = 1;
                         }
                     }
                     match = 0;
@@ -51,7 +55,9 @@ Array.from(document.getElementsByClassName("col-box")).forEach((box) => {
                         }
                         if (match == 3) {
                             console.log("Player 1 won the game.");
-                            bingo = 1;
+                            document.getElementById("player-1").innerHTML = "Player 1 WON [circle]";
+                            document.getElementById("player-2").innerHTML = "Player 2 LOSE [Cross]";
+                            finish = 1;
                         }
                     }
                     match = 0;
@@ -61,3 +67,17 @@ Array.from(document.getElementsByClassName("col-box")).forEach((box) => {
         }
     });
 });
+
+//on reset game
+document.getElementById("reset").onclick = function () {
+    Array.from(document.getElementsByClassName("col-box")).forEach((box) => {
+        box.innerHTML = "";
+        turn = 1; //for selecting, player turn
+        pos1 = pos1.slice(pos1.length);
+        pos2 = pos2.slice(pos2.length);
+        match = 0;
+        finish = 0;
+        document.getElementById("player-1").innerHTML = "Player 1 [Circle]";
+        document.getElementById("player-2").innerHTML = " Player 2 [Cross]";
+    });
+}
